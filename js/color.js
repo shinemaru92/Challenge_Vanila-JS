08a4d853784d04c803f19bcd78b187ab22c4c2ed
shinemaru <kfcs2002@gmail.com>
2023-06-26 05:57:44 +0900

    230626

---
+++ js/color.js
@@ -0,0 +1,21 @@
+// // 버튼 요소 가져오기
+// const button = document.getElementById("colorButton");
+
+// // 랜덤 그라데이션 배경색을 설정하는 함수
+// function setRandomGradient() {
+//   const color1 = getRandomColor();
+//   const color2 = getRandomColor();
+//   const gradient = `linear-gradient(to right, ${color1}, ${color2})`;
+//   document.body.style.background = gradient;
+// }
+
+// // 랜덤 RGB 색상을 반환하는 함수
+// function getRandomColor() {
+//   const red = Math.floor(Math.random() * 256);
+//   const green = Math.floor(Math.random() * 256);
+//   const blue = Math.floor(Math.random() * 256);
+//   return `rgb(${red}, ${green}, ${blue})`;
+// }
+
+// // 버튼 클릭 이벤트 핸들러 등록
+// button.addEventListener("click", setRandomGradient);
