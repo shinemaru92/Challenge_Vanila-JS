212ac88d92e7ca17874f740f031ff74398d6144b
shinemaru92 <kfcs2002@gmail.com>
2023-11-10 11:04:55 +0900

    231110

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "4e57782cdfc7912abf219c629498200e";
+
 function onGeoOk(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const log = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = `내 지역: ${data.name}`;
+      weather.innerText = `오늘 날씨: ${data.weather[0].main}`;
+    }); //promise는 당장 뭔가 일어나지 않고 시간이 좀 걸린 뒤 일어난다.
+  //url에 나오는 모든 내용이 JSON이다.
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
